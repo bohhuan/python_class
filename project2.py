c54c63df2cb5030815822f2db320d17b47d1e1eb
bohhuan <bohhuang206@gmail.com>
2023-08-12 11:46:06 -0700

    add project2.py

---
+++ project2.py
@@ -0,0 +1,29 @@
+import random
+
+"""
+python has built-in library, offical document link @ https://docs.python.org/3/library/index.html
+here is a simple example to demo how to use module 'random'.
+"""
+def guess_number(start, end):
+    """
+    let user to guess a integer number belonging to the range [start, end].
+    """
+    # generate one secret number first, and then let users try to guess it.
+    secret_number = random.randint(start, end)
+    user_input_number = -1
+    
+    print(f"lets guess the number between {start} and {end} !")
+
+    # keep loop until user guess the corrected number successfully
+    while user_input_number != secret_number:
+        user_input_number = int(input("try your number > "))
+
+        if user_input_number < secret_number:
+            print("too small, try big number !")
+        elif user_input_number > secret_number:
+            print("too big, try small number !")
+
+    print(f"congras ! you guess the correct number {secret_number} successfully !")
+
+# now, it is time to play !
+guess_number(0, 10)
